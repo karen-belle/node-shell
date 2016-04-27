47b7603b860f3bda435ef2cef48bb1346f3cd9db
Karen Abramowitz <karenabramowitz@gmail.com>
2016-04-27 11:46:39 -0400

    Piping completed

--- bash.js
+++ bash.js
@@ -5,20 +5,27 @@ var commandObj = require('./commands');
 
 process.stdout.write('prompt > ');
 
-
+var cmdList = [];
 // The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
-  var cmdArray = cmd.split(" ");
-  var options = cmdArray.slice(1);
+process.stdin.on('data', function(data) {
+    var cmd = data.toString().trim(); // remove the newline
+    var cmdArray = cmd.split(" ");
+    cmdList = cmd.split(/\s*\|\s*/g).slice(1);
 
-  commandObj[cmdArray[0]](done, options);
-  //process.stdout.write('\nprompt > ');
+    var newArray = cmdArray.slice(1);
+
+    //console.log(options);
+
+    commandObj[cmdArray[0]](done, newArray);
+    //process.stdout.write('\nprompt > ');
 });
 
 var done = function(output) {
-  // show the output
-  process.stdout.write(output);
-  // show the prompt
-  process.stdout.write('\nprompt > ');
+	   
+	process.stdout.write( output);
+    if(cmdList.length)
+    {
+    	commandObj[cmdList.pop()](done, null, output);
+    }
+    process.stdout.write('\nprompt > ');
 }
--- commands.js
+++ commands.js
@@ -23,148 +23,201 @@ module.exports = {
 
     },
 
-    find: function(done, array) {
-        //console.log("Array ", array[0]);
-        var output = "";
-        var readDirectories = [];
-		//var curr = "";
+    //   find: function(done, array) {
+    //       //console.log("Array ", array[0]);
+    //       var output = "";
+    //       var readDirectories = [];
+    // //var curr = "";
 
-        function list(dir) {
-            //console.log("Directory ", dir);
-            fs.readdir(dir, function(err, files) {
-                if (err) throw err;
-                files.forEach(function(file) {
-                    console.log(file.toString());
-                    readDirectories.push(file.toString());
-                    //var parent =  array[0] + "/" + curr; 
-                    
-                    while (readDirectories.length > 0) {
-                    	//We'll need to keep adding the path of the parent directory for as long as we are recursing
-                        //curr = parent + "/" + readDirectories.pop();
-                        var curr = array[0] + "/" + readDirectories.pop();
-                        if(!curr.match(/[\.\s]/g))
-                        {
-                            list(curr);
-                        }
-                        //}
-                    }
-                });
-                //console.log("The queue 2", readDirectories);
-            });
-        }
-        list(array[0]);
-        //console.log("The queue 1", readDirectories);
+    //       function list(dir) {
+    //           //console.log("Directory ", dir);
+    //           fs.readdir(dir, function(err, files) {
+    //               if (err) throw err;
+    //               files.forEach(function(file) {
+    //                   console.log(file.toString());
+    //                   readDirectories.push(file.toString());
+    //                   //var parent =  array[0] + "/" + curr; 
 
+    //                   while (readDirectories.length > 0) {
+    //                      //We'll need to keep adding the path of the parent directory for as long as we are recursing
+    //                       //curr = parent + "/" + readDirectories.pop();
+    //                       var curr = array[0] + "/" + readDirectories.pop();
+    //                       if(!curr.match(/[\.\s]/g))
+    //                       {
+    //                           list(curr);
+    //                       }
+    //                       //}
+    //                   }
+    //               });
+    //               //console.log("The queue 2", readDirectories);
+    //           });
+    //       }
+    //       list(array[0]);
+    //       //console.log("The queue 1", readDirectories);
 
+    //       done(output);
 
-        done(output);
-
-    },
+    //   },
 
 
     echo: function(done, array) {
         var output = array.join(" ");
         done(output);
     },
-    cat: function(done, array) {
-
-        fs.readFile(array[0], function(err, data) {
-            if (err) throw err;
-            var output = data;
+    cat: function(done, array, stdin) {
+        var output = "";
+        // console.log("Here's our cat array ", array);
+        // console.log("Here's our cat stdin ", stdin);
+        if (stdin !== undefined) {
+            // console.log("we shouldn't be here.");
+            output = stdin.toString();
             done(output);
+        } else {
+            // console.log("Let's read that file, meow.");
+            fs.readFile(array[0], function(err, data) {
+                if (err) throw err;
+                output = data;
+                done(output);
+            });
+        }
+        //done(output);
 
-        });
     },
 
-    head: function(done, array) {
+    head: function(done, array, stdin) {
         var output = "";
-
-        fs.readFile(array[0], function(err, data) {
-
-            if (err) {
-                throw err;
-            }
-            output = "";
-            var lines = data.toString().split('\n');
+        //console.log("Here's our head input ", stdin);
+        if (stdin !== undefined) {
+            var lines = stdin.toString().split('\n');
             for (var i = 0; i < 5; i++) {
                 output += lines[i] + '\n';
             }
             done(output);
+        } else {
+            fs.readFile(array[0], function(err, data) {
 
-        });
+                if (err) {
+                    throw err;
+                }
+                output = "";
+                var lines = data.toString().split('\n');
+                for (var i = 0; i < 5; i++) {
+                    output += lines[i] + '\n';
+                }
+                done(output);
+            });
+        }
+
     },
 
-    tail: function(done, array) {
+    tail: function(done, array, stdin) {
         var output = ""
-        fs.readFile(array[0], function(err, data) {
-
-            if (err) {
-                throw err;
-            }
-
-            var lines = data.toString().split('\n');
+        if (stdin !== undefined) {
+            var lines = stdin.toString().split('\n');
             for (var i = lines.length - 5; i < lines.length; i++) {
                 output += lines[i] + '\n';
             }
             done(output);
+        } else {
+            fs.readFile(array[0], function(err, data) {
 
-        });
+                if (err) {
+                    throw err;
+                }
+
+                var lines = data.toString().split('\n');
+                for (var i = lines.length - 5; i < lines.length; i++) {
+                    output += lines[i] + '\n';
+                }
+                done(output);
+
+            });
+        }
     },
 
-    sort: function(done, array) {
+    sort: function(done, array, stdin) {
         var output = "";
-        fs.readFile(array[0], function(err, data) {
-
-            if (err) {
-                throw err;
-            }
-
-            var lines = data.toString().split('\n').sort();
+        if (stdin !== undefined) {
+            var lines = stdin.toString().split('\n').sort();
             for (var i = 0; i < lines.length; i++) {
                 output += lines[i] + '\n';
             }
             done(output);
+        } else {
 
-        });
+            fs.readFile(array[0], function(err, data) {
+
+                if (err) {
+                    throw err;
+                }
+
+                var lines = data.toString().split('\n').sort();
+                for (var i = 0; i < lines.length; i++) {
+                    output += lines[i] + '\n';
+                }
+                done(output);
+
+            });
+        }
     },
 
-    wc: function(done, array) {
-        //console.log(array[0]);
-        fs.readFile(array[0], function(err, data) {
-
-            if (err) {
-                throw err;
-            }
-
-            var lines = data.toString().split('\n');
-
-            var output = array[0] + " has " + lines.length + " lines.";
-
+    wc: function(done, array, stdin) {
+        var output = "";
+        if (stdin !== undefined) {
+            var lines = stdin.toString().split('\n');
+            output = "Input has " + lines.length + " lines.";
             done(output);
+        } else {
 
-        });
+            fs.readFile(array[0], function(err, data) {
+
+                if (err) {
+                    throw err;
+                }
+
+                var lines = data.toString().split('\n');
+
+                output = array[0] + " has " + lines.length + " lines.";
+                done(output);
+
+            });
+        }
     },
     //console.log(array[0]);
-    uniq: function(done, array) {
-        fs.readFile(array[0], function(err, data) {
-
-            if (err) {
-                throw err;
-            }
-
-            var lines = data.toString().split('\n').sort();
+    uniq: function(done, array, stdin) {
+        var output = "";
+        if (stdin !== undefined) {
+            var lines = stdin.toString().split('\n').sort();
             var linesToPrint = [];
             for (var i = 1; i <= lines.length; i++) {
                 if (lines[i] != lines[i - 1]) {
                     linesToPrint.push(lines[i - 1]);
                 }
             }
-
-            var output = linesToPrint.join('\n');
-
+            output = linesToPrint.join('\n');
             done(output);
+        } 
+        else {
 
-        });
+            fs.readFile(array[0], function(err, data) {
+
+                if (err) {
+                    throw err;
+                }
+
+                var lines = data.toString().split('\n').sort();
+                var linesToPrint = [];
+                for (var i = 1; i <= lines.length; i++) {
+                    if (lines[i] != lines[i - 1]) {
+                        linesToPrint.push(lines[i - 1]);
+                    }
+                }
+
+                output = linesToPrint.join('\n');
+                done(output);
+
+            });
+        }
     },
 
     curl: function(done, array) {
